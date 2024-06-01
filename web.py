9199d13a69a4163937ed9037086a46b6ba7500a1
AVRSANAND <avrsanand2003@gmail.com>
2024-06-01 22:45:10 +0530

    Select files and destination folder

--- web.py
+++ web.py
@@ -1,11 +1,31 @@
 import streamlit as st
+import tkinter as tk
+from tkinter import filedialog
 import functions
 
 st.title("Streamlit Web App")
 st.write("Convert Files to Zip Archives OR Extract Zip Archives.")
 
-upload_files = st.file_uploader("Upload Files", accept_multiple_files=True, key="files")
-dest_folder = st
+
+root = tk.Tk()
+root.withdraw()
+
+# Make folder picker dialog appear on top of other windows
+root.wm_attributes('-topmost', 1)
+
+# upload_files = st.file_uploader("Upload Files", accept_multiple_files=True, key="files")
+upload_files = st.button("Upload Files")
+
+if upload_files:
+    filepaths = filedialog.askopenfilenames(master=root)
+    st.write(filepaths)
+# Folder picker button
+st.write('Please select a destination folder:')
+clicked = st.button('Folder Picker')
+
+if clicked:
+    dirname = st.text_input('Selected folder:', filedialog.askdirectory(master=root))
+
 compress = st.button("Compress")
 
 upload_file = st.file_uploader("Upload File", type=["zip"], accept_multiple_files=False, key="file")
